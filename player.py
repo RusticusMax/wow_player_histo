fe6c67c15836e5e082683d13e81b9554a81c4f4a
RusticusMax <rusticusmax@gmail.com>
2022-12-28 13:06:23 -0500

    Add selenium/Chromedriver to render web page and query that.  Chrome still shows a UI.  Would like this to be all in memory

--- player.py
+++ player.py
@@ -5,17 +5,31 @@
 from lxml import html
 import requests
 import sys  # contains stderr object for debug output
+import time
+
+# pip install selenium
+from selenium import webdriver
+# pip install python-blizzardapi
+
 
 class Player:
     def __init__(self, URL):
-        p_page = requests.get(URL)  # slurp page from internet
-        print(p_page.status_code, len(p_page.content)) # Need reliable check for successful page slurp
-        p_tree = html.fromstring(p_page.content)
+        driver = webdriver.Chrome('C:\\Program Files (x86)\\Goggle-ChromeDriver\\chromedriver.exe')
+        driver.get(URL);
+        time.sleep(5)
+        p_name = driver.find_element_by_xpath('//*[@id="character-profile-mount"]/div/div/div[2]/div/div[1]/div[1]/div/div[1]/div[2]/div/a')
+        p_title = driver.find_element_by_xpath('//meta[@name="description"]')
+        p_spec = driver.find_element_by_xpath('//*[@class="CharacterHeader-detail"]/span[3]')
+
+        pass
+
+        #p_page = requests.get(URL)  # slurp page from internet
+        #print(p_page.status_code, len(p_page.content)) # Need reliable check for successful page slurp
+        #p_tree = html.fromstring(p_page.content)
 
         ## Page is just scripts to build page
 
-        p_title = p_tree.xpath('//meta[@name="description"]')
-        p_name = p_tree.xpath('//*[@id="character-profile-mount"]/div/div/div[2]/div/div[1]/div[1]/div/div[1]/div[2]/div/a')
-        p_spec = p_tree.xpath('//*[@class="CharacterHeader-detail"]/span[3]')
-        print(len(p_name), len(p_spec), len(p_title))
-        pass
+        #p_title = p_tree.xpath('//meta[@name="description"]')
+        #p_name = p_tree.xpath('//*[@id="character-profile-mount"]/div/div/div[2]/div/div[1]/div[1]/div/div[1]/div[2]/div/a')
+        #p_spec = p_tree.xpath('//*[@class="CharacterHeader-detail"]/span[3]')
+        print(p_name.text, p_spec.text, len(p_title.text))
