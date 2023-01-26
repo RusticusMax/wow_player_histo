dddcbdc65f424b1c31d52d86237945a626b38dba
RusticusMax <rusticusmax@gmail.com>
2023-01-26 10:01:37 -0500

    Changed to dict of counters in stead of array of dicts.  Still seems like we should not need to dup strings. Either join dicts (offset counter buckets?) or parallel array

--- pvp_player_histo.py
+++ pvp_player_histo.py
@@ -24,34 +24,42 @@ HISTO_TOP_X = 5
 PLAYER_MAX = HISTO_BAR_COUNT * HISTO_HEIGHT
 
 # Dictionary for counting classes
-class_list = {' Windwalker Monk': 0,        ' Holy Paladin': 0,          ' Frost Death Knight': 0,
-              ' Restoration Druid': 0,      ' Survival Hunter': 0,       ' Frost Mage': 0,
-              ' Restoration Shaman': 0,     ' Retribution Paladin': 0,   ' Shadow Priest': 0,
-              ' Arms Warrior': 0,           ' Elemental Shaman': 0,      ' Feral Druid': 0,
-              ' Balance Druid': 0,          ' Destruction Warlock': 0,   ' Affliction Warlock': 0,
-              ' Subtlety Rogue': 0,         ' Mistweaver Monk': 0,       ' Unholy Death Knight': 0,
-              ' Assassination Rogue': 0,    ' Protection Paladin': 0,    ' Discipline Priest': 0,
-              ' Enhancement Shaman': 0,     ' Arcane Mage': 0,           ' Demonology Warlock': 0,
-              ' Guardian Druid': 0,         ' Havoc Demon Hunter': 0,    ' Fire Mage': 0,
-              ' Marksmanship Hunter': 0,    ' Holy Priest': 0,           ' Outlaw Rogue': 0,
-              ' Beast Mastery Hunter': 0,   ' Protection Warrior': 0,    ' Blood Death Knight': 0,
-              ' Vengeance Demon Hunter': 0, ' Fury Warrior': 0,          ' Brewmaster Monk': 0,
-              ' Preservation Evoker': 0,    ' Devastation Evoker': 0,
+class_list = {'Windwalker Monk': 0,        'Holy Paladin': 0,          'Frost Death Knight': 0,
+              'Restoration Druid': 0,      'Survival Hunter': 0,       'Frost Mage': 0,
+              'Restoration Shaman': 0,     'Retribution Paladin': 0,   'Shadow Priest': 0,
+              'Arms Warrior': 0,           'Elemental Shaman': 0,      'Feral Druid': 0,
+              'Balance Druid': 0,          'Destruction Warlock': 0,   'Affliction Warlock': 0,
+              'Subtlety Rogue': 0,         'Mistweaver Monk': 0,       'Unholy Death Knight': 0,
+              'Assassination Rogue': 0,    'Protection Paladin': 0,    'Discipline Priest': 0,
+              'Enhancement Shaman': 0,     'Arcane Mage': 0,           'Demonology Warlock': 0,
+              'Guardian Druid': 0,         'Havoc Demon Hunter': 0,    'Fire Mage': 0,
+              'Marksmanship Hunter': 0,    'Holy Priest': 0,           'Outlaw Rogue': 0,
+              'Beast Mastery Hunter': 0,   'Protection Warrior': 0,    'Blood Death Knight': 0,
+              'Vengeance Demon Hunter': 0, 'Fury Warrior': 0,          'Brewmaster Monk': 0,
+              'Preservation Evoker': 0,    'Devastation Evoker': 0,
               }
 
-# todo: switch to this so we don't need parallel dictionaries
-# test_dict = { ' Windwalker Monk': [0] * HISTO_HEIGHT,        ' Holy Paladin': [0] * HISTO_HEIGHT,
-#               ' Frost Death Knight': [0] * HISTO_HEIGHT, }
-# test_dict[' Windwalker Monk'][2] = 7
-
-# todo:    create clear list objects to better handle parallel dictionaries
-#  , or A dictionary of lists HISTO_BAR_COUNT wide   " class spec": list[
+# Buckets for counting
+H = HISTO_BAR_COUNT
+histo_dict = {'Windwalker Monk': [0] * H,          'Holy Paladin': [0] * H,        'Frost Death Knight': [0] * H,
+              'Restoration Druid': [0] * H,        'Survival Hunter': [0] * H,     'Frost Mage': [0] * H,
+              'Restoration Shaman': [0] * H,       'Retribution Paladin': [0] * H, 'Shadow Priest': [0] * H,
+              'Arms Warrior': [0] * H,             'Elemental Shaman': [0] * H,    'Feral Druid': [0] * H,
+              'Balance Druid': [0] * H,            'Destruction Warlock': [0] * H, 'Affliction Warlock': [0] * H,
+              'Subtlety Rogue': [0] * H,           'Mistweaver Monk': [0] * H,     'Unholy Death Knight': [0] * H,
+              'Assassination Rogue': [0] * H,      'Protection Paladin': [0] * H,  'Discipline Priest': [0] * H,
+              'Enhancement Shaman': [0] * H,       'Arcane Mage': [0] * H,         'Demonology Warlock': [0] * H,
+              'Guardian Druid': [0] * H,           'Havoc Demon Hunter': [0] * H,  'Fire Mage': [0] * H,
+              'Marksmanship Hunter': [0] * H,      'Holy Priest': [0] * H,         'Outlaw Rogue': [0] * H,
+              'Beast Mastery Hunter': [0] * H,     'Protection Warrior': [0] * H,  'Blood Death Knight': [0] * H,
+              'Vengeance Demon Hunter': [0] * H,   'Fury Warrior': [0] * H,        'Brewmaster Monk': [0] * H,
+              'Preservation Evoker': [0] * H,      'Devastation Evoker': [0] * H,
+              }
 
 # Module level variable are always treated as constants, tell pylint to ignore const case requirement for the following
 # pylint: disable=C0103
 # create an array of dictionaries for histogram data
 # copy each (so we don't end up with an array of references) class_list dictionary for each histo bar
-histo_buckets = [class_list.copy() for x in range(HISTO_BAR_COUNT)]
 player_current = 0  # init current player counter so we will know when to stop
 page_count = 1  # So we can request the right page of data from web site, since we have to do it a page at a time.
 # pylint: enable=C0103
@@ -69,7 +77,7 @@ while player_current < PLAYER_MAX:
     players = tree.xpath('//div[@class="Character-name"]/text()')
     realms = tree.xpath('//div[@class="Character-realm"]/text()')
     # How does this  extract the actual name, and not also the level number?
-    player_class = tree.xpath('//div[@class="Character-level"]/text()')
+    player_class = [x.strip() for x in tree.xpath('//div[@class="Character-level"]/text()')]
 
     # did we get data?  If not skip. But track number of fail and exit if reached limit (BLANK_PAGE_CNT)
     if len(players) == 0:
@@ -97,10 +105,12 @@ while player_current < PLAYER_MAX:
                     print("Unicode Error on print")
                     sys.exit(2)
                 except Exception as e:
+                    print("Exception on print", e.args)
                     oops = e
             class_list[i_player_class] += 1
             # inc appropriate histogram bucket
-            histo_buckets[int((player_current - 1) / HISTO_HEIGHT)][i_player_class] += 1
+#            histo_buckets[int((player_current - 1) / HISTO_HEIGHT)][i_player_class] += 1
+            histo_dict[i_player_class][int((player_current - 1) / HISTO_HEIGHT)] += 1
 
     page_count += 1
 
@@ -123,15 +133,24 @@ for class_item in sorted(class_list.keys(), key=lambda class_str: class_list[cla
 
 # Output header
 print("Rank", ',', end='')
-for histo_item in histo_top_classes:
-    print(histo_item, ',', end='')
+for class_item in histo_top_classes:
+    print(class_item, ',', end='')
 print()  # And a newline to end
 
 # Print actual histogram data
 # for each dictionary in the array process that batch of counts
-for i, histo_bar in enumerate(histo_buckets):
+# for i, histo_bar in enumerate(histo_buckets):
+#     # Print left legend for bucket ranges (e.g. 0-20, 21-40, etc)
+#     print((i * HISTO_HEIGHT) + 1, " ", (i + 1) * HISTO_HEIGHT, ',', end='')
+#     for histo_item in histo_top_classes:
+#         print(histo_bar[histo_item],  ',', end='')
+#     print()     # end of line
+#
+# print("----------------------------------------")
+
+for i in range(HISTO_BAR_COUNT):
     # Print left legend for bucket ranges (e.g. 0-20, 21-40, etc)
     print((i * HISTO_HEIGHT) + 1, " ", (i + 1) * HISTO_HEIGHT, ',', end='')
     for histo_item in histo_top_classes:
-        print(histo_bar[histo_item],  ',', end='')
-    print()     # end of line
+        print(histo_dict[histo_item][i], ',', end='')
+    print()  # end of line
